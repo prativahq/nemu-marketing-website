6265fb575fdd1182ac3295d5b72358ecab2e2f19
kVarunkk <varunkumawatleap2@gmail.com>
2023-12-10 02:44:10 +0530

    add netlify cms

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
-// import React from "react";
-// // import { createRoot } from 'react-dom/client';
-// import ReactDOM from "react-dom";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-
-// const container = document.getElementById("root");
-// // const root = createRoot(container);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./index.css";
+import IndexPage from "./pages/Index.js";
+import HowItWorksPage from "./pages/How-it-works.js";
+import TermsOfServicePage from "./pages/Terms-of-service.js";
+import PrivacyPolicyPage from "./pages/Privacy-policy.js";
+import LegalPage from "./pages/Legal.js";
+import RefundsCancellationPage from "./pages/Refunds-cancellation.js";
+import ContactUsPage from "./pages/Contact-us.js";
+import PartnerPage from "./pages/Partner.js";
+import AboutUsPage from "./pages/About-us.js";
+import BlogPage from "./pages/Blog.js";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+
+        <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+
+        <Route path="/legal" element={<LegalPage />} />
+
+        <Route
+          path="/refunds-cancellation"
+          element={<RefundsCancellationPage />}
+        />
+
+        <Route path="/contact-us" element={<ContactUsPage />} />
+
+        <Route path="/partner" element={<PartnerPage />} />
+
+        <Route path="/about-us" element={<AboutUsPage />} />
+
+        <Route path="/blog" element={<BlogPage />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
