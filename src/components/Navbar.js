f13f5da6d286e42e9479ff2ec8d9009b09c27730
kVarunkk <varunkumawatleap2@gmail.com>
2023-12-12 17:11:13 +0530

    add accesibility zoom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,16 @@ import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  // useEffect(() => {
-  //   const script = document.createElement("script");
-
-  //   script.src = "assets/js/main.js";
-  //   script.async = true;
-
-  //   document.body.appendChild(script);
-
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
-
   const [toggle, setToggle] = useState(false);
+
   const path = window.location.pathname;
   console.log(path);
+
+  const toggleZoom = () => {
+    if (document.body.style.zoom === "100%") {
+      document.body.style.zoom = "120%";
+    } else document.body.style.zoom = "100%";
+  };
 
   return (
     <section className="shadow-inner shadow-[#EAEBEB] bg-white font-body">
@@ -99,7 +93,10 @@ export const Navbar = () => {
             </ul>
           </div>
           <div className="hidden xl:block xl:w-1/4">
-            <div className="flex items-center justify-end">
+            <div className="flex gap-5 items-center justify-end">
+              <button onClick={toggleZoom}>
+                <img src="images/zoom-nav.svg" />
+              </button>
               <a
                 className="inline-block py-3 px-5 font-semibold  leading-5 text-white bg-emerald-500 hover:bg-emerald-600 text-lg focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-50 rounded-md"
                 href="https://share.hsforms.com/1Vj_1vX50RUSNaNpFPu969Qq6kwws"
@@ -109,31 +106,17 @@ export const Navbar = () => {
             </div>
           </div>
         </div>
-        <button
-          onClick={() => setToggle(!toggle)}
-          className="navbar-burger self-center xl:hidden"
-        >
-          <svg
-            width={35}
-            height={35}
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="flex gap-6">
+          <button className="xl:hidden" onClick={toggleZoom}>
+            <img src="images/zoom-nav.svg" />
+          </button>
+          <button
+            onClick={() => setToggle(!toggle)}
+            className="navbar-burger self-center xl:hidden"
           >
-            <rect
-              className="text-coolGray-50"
-              width={32}
-              height={32}
-              rx={6}
-              fill="currentColor"
-            />
-            <path
-              className="text-coolGray-500"
-              d="M7 12H25C25.2652 12 25.5196 11.8946 25.7071 11.7071C25.8946 11.5196 26 11.2652 26 11C26 10.7348 25.8946 10.4804 25.7071 10.2929C25.5196 10.1054 25.2652 10 25 10H7C6.73478 10 6.48043 10.1054 6.29289 10.2929C6.10536 10.4804 6 10.7348 6 11C6 11.2652 6.10536 11.5196 6.29289 11.7071C6.48043 11.8946 6.73478 12 7 12ZM25 15H7C6.73478 15 6.48043 15.1054 6.29289 15.2929C6.10536 15.4804 6 15.7348 6 16C6 16.2652 6.10536 16.5196 6.29289 16.7071C6.48043 16.8946 6.73478 17 7 17H25C25.2652 17 25.5196 16.8946 25.7071 16.7071C25.8946 16.5196 26 16.2652 26 16C26 15.7348 25.8946 15.4804 25.7071 15.2929C25.5196 15.1054 25.2652 15 25 15ZM25 20H7C6.73478 20 6.48043 20.1054 6.29289 20.2929C6.10536 20.4804 6 20.7348 6 21C6 21.2652 6.10536 21.5196 6.29289 21.7071C6.48043 21.8946 6.73478 22 7 22H25C25.2652 22 25.5196 21.8946 25.7071 21.7071C25.8946 21.5196 26 21.2652 26 21C26 20.7348 25.8946 20.4804 25.7071 20.2929C25.5196 20.1054 25.2652 20 25 20Z"
-              fill="currentColor"
-            />
-          </svg>
-        </button>
+            <img src="images/hamburger-menu.svg" />
+          </button>
+        </div>
       </nav>
       <div
         className={`navbar-menu ${
